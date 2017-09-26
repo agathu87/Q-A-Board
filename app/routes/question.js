25d262dc775a2be0d14a7a844fe47a9af3b5a045
Angela Gathu <agathu87@gmail.com>
2017-09-26 10:16:01 +0300

    update file

---
+++ app/routes/question.js
@@ -0,0 +1,16 @@
+export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('question', params.question_id);
+  },
+  actions: {
+    saveNewAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        question.save()
+      });
+      this.transitionTo('question')
+    },
+  }
+});
