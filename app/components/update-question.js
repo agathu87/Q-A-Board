7fcf72a8edff283bd10bec5b09a16020d49ecc84
Angela Gathu <agathu87@gmail.com>
2017-09-26 10:47:22 +0300

    update file

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,5 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  showUpdateQuestion: false,
+  actions: {
+    showUpdateQuestion() {
+      this.set('showUpdateQuestion', true)
+    },
+    hideUpdateQuestion() {
+      this.set('showUpdateQuestion', false),
+        this.set('actualQuestion', '')
+    },
+    updateQuestion() {
+      var question = this.get('question');
+      var params = {
+        actualQuestion: this.get('actualQuestion'),
+      };
+      this.sendAction('updateQuestion1', question, params),
+        this.set('showUpdateQuestion', false)
+    }
+  }
 });
